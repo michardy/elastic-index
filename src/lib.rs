d70951d75c47fd5b78f0d4121683596559b0fa07
Michael Hardy <98.michael.hardy@gmail.com>
2018-10-23 23:12:52 -0700

    Basic indexing API

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,49 @@
+extern crate reqwest;
+extern crate serde;
+extern crate serde_json;
+
+use serde::Serialize;
+
+/// Client object to connect to Elasticsearch REST interface.
+/// Object stores Hyper Client instance
+pub struct Client {
+	client: reqwest::Client,
+	host: String
+}
+
+impl Client {
+	pub fn new(host: String) -> Client {
+		Client {
+			client: reqwest::Client::new(),
+			host: host
+		}
+	}
+}
+
+/// Elasticsearch index object
+pub struct Index {
+	client: Client,
+	name: String
+}
+
+impl Index {
+	/// Create new index object with client and index name.
+	/// This does not result in index creation
+	pub fn new(client: Client, name: String) -> Index {
+		Index {
+			client: client,
+			name: name,
+		}
+	}
+	/// Index an object to create Elasticsearch document
+	pub fn index<T: Serialize>(self, doc: T) {
+		let url = format!("{}/{}/_doc", self.client.host, self.name);
+		self.client.client.put(&url)
+			.body(serde_json::to_string(&doc).unwrap())
+			.send().unwrap();
+	}
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
